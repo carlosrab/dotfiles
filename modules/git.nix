45d2c53a51221414d642384f458e3092d30634fa
carlos <49134466+carlosrab@users.noreply.github.com>
2024-07-19 23:08:06 -0700

    git configuration additions.
    Includes basic aliases, and generic ignores.

--- modules/git.nix
+++ modules/git.nix
@@ -5,8 +5,25 @@
     userName = "carlos";
     userEmail = "49134466+carlosrab@users.noreply.github.com";
 
+    aliases = {
+      gcm = "commit";
+      gca = "commit --amend --no-edit";
+      gco = "checkout";
+
+      gdi = "diff";
+
+      gpu = "pull";
+      gps = "push";
+
+      gst = "status -sb";
+
+      gri = "rebase -i";
+      grc = "rebase --continue";
+    };
+
     delta = {
       enable = true;
+      line-numbers = true;
       options = {
         syntax-theme = "solarized-dark";
         side-by-side = true;
@@ -25,5 +42,24 @@
       core.fileMode = false;
       core.ignorecase = false;
     };
+
+    ignores = [
+      # ide
+      ".idea"
+      ".vs"
+      ".vsc"
+      ".vscode"
+      # npm
+      "node_modules"
+      "npm-debug.log"
+      # python
+      "__pycache__"
+      "*.pyc"
+
+      ".ipynb_checkpoints" # jupyter
+      "__sapper__" # svelte
+      ".DS_Store" # mac
+      "kls_database.db" # kotlin lsp
+    ];
   };
 }
