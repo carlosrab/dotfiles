888140332d7234d825c7b567d6eb276cdf7d865c
carlos <49134466+carlosrab@users.noreply.github.com>
2024-07-19 23:08:06 -0700

    very basic setup with nix flakes.

--- mbp-lima.nix
+++ mbp-lima.nix
@@ -1,10 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   # This is required information for home-manager to do its job
   home = {
     stateVersion = "23.11";
     username = "carlos";
     homeDirectory = "/Users/carlos";
-    packages = [];
+    packages = [
+      pkgs.git
+      pkgs.neofetch
+    ];
   };
+  xdg.enable = true;
+
   programs.home-manager.enable = true;
 }
